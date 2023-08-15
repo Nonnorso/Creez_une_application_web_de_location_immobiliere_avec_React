a03fe03bcdaffa4d87c86e09064f4d724c485938
Nonnorso <r.philippot@live.fr>
2023-08-15 11:39:33 +0200

    integration de la page 404

--- src/pages/404/error404.jsx
+++ src/pages/404/error404.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+import '../../Styles/index.scss'
+
 function Error404() {
     return (
-        <div>
-            <h1>ERROR 404</h1>
+        <div className="FlexColumn Style404">
+            <h1 className="RedText Title404">404</h1>
+            <p className="RedText Txt404">Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="RedText SmallText" to="/" >Retourner sur la page d’accueil</Link>
         </div>
     )
 }
