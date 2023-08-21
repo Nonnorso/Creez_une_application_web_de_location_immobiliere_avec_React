a0ae83a7d2598a360d4c5a5e6c802657f3055131
Nonnorso <r.philippot@live.fr>
2023-08-21 17:02:10 +0200

    responsive page a propos avec animations et correction du bug de l'icone

--- src/pages/a_propos/aPropos.jsx
+++ src/pages/a_propos/aPropos.jsx
@@ -1,6 +1,6 @@
 import AboutBanner from '../../components/About_Banner'
 import Collapse from '../../components/Collapse';
-import collapseIcon from '../../assets/arrow_back_ios-24px 2.svg';
+import collapseIcon from '../../assets/arrow_back_ios-24px 2.png';
 
 function aPropos() {
     return (
@@ -13,7 +13,7 @@ function aPropos() {
             content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
             régulièrement vérifiées par nos équipes."
           />
-          
+
           <Collapse
             title="Respect"
             icon={<img src={collapseIcon} alt="Icône de Collapse" />}
