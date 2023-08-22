babae511c7eca6821f0c160df1c322dd7ca37fbd
Nonnorso <r.philippot@live.fr>
2023-08-22 14:53:07 +0200

    modification de l'import avec Fecth pour que les deux manieres soient fonctionnelles

--- src/components/Router/AppRouter.jsx
+++ src/components/Router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '../../pages/home/App';
 import APropos from '../../pages/a_propos/aPropos';
@@ -6,15 +6,25 @@ import Error404 from '../../pages/404/error404';
 import Header from '../../components/Header/Navigation';
 import Footer from '../../components/Footer/FooterInfos';
 import CheckLogement from '../CheckLogement';
-import JsonData from '../ExportData';
+import jsonData from '../../datas/Annonces.json';
 
 function AppRouter() {
+
+  // const [jsonData, setJsonData] = useState([]); // État local pour les données JSON
+
+  // useEffect(() => {
+  //   fetch('/Annonces.json')
+  //     .then(response => response.json())
+  //     .then(data => setJsonData(data))
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
+  
     return (
       <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<App jsonData={JsonData} />} />
-        <Route path="/logement/:id" element={<CheckLogement jsonData={JsonData} />} />
+        <Route path="/" element={<App jsonData={jsonData} />} />
+        <Route path="/logement/:id" element={<CheckLogement jsonData={jsonData} />} />
         <Route path="/about" element={<APropos />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
