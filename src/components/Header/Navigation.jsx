6db0b247cd82138a072ad683ddb753c604e5c5b4
Nonnorso <r.philippot@live.fr>
2023-08-24 15:35:33 +0200

    ajout fonctionnalité de redirection au click sur le logo de la banniere

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -5,8 +5,10 @@ import Logo from '../../assets/LOGO.svg'
 function Header() {
     return (
         <nav className="FlexRow NavStyle">
-            <img src={Logo} alt="Logo Kasa" title="Logo Kasa" />
-        
+            <Link to="/">
+                <img src={Logo} alt="Logo Kasa" title="Logo Kasa" />
+            </Link>
+            
             <div className="LinkStyle">
                 <Link className="MediumSizeText RedText" to="/" >Accueil</Link>
                 <Link className="MediumSizeText RedText" to="/about" >A Propos</Link>
